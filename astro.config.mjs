4b391fe8ec37ebd4d5769b17310561b3be4f067a
Pretty Git <132768132+Ikhode-kh@users.noreply.github.com>
2024-08-17 18:25:51 +0700

    sitemap seo test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap(), ]
+  site: 'https://ikhode.site',
+  integrations: [tailwind(), sitemap()]
 });
